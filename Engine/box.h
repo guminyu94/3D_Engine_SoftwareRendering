f9ebb46c57f4f736a8bb16c88369990601c0925e
Minyu Gu <guminyu94@gmail.com>
2019-03-19 17:16:18 -0500

    Revert "0"
    This reverts commit 5d9d8908fe8c415dc4083daea2cbd1e54d8522ff.

--- Engine/box.h
+++ Engine/box.h
@@ -3,16 +3,16 @@
 #include <vector>
 #include "Vec3.h"
 #include "VertexIndexList.h"
-#include "Vertex.h"
-
+#include "JPG2Vector.h"
+#include <string>
 
 class box
 {
 public:
-	box(const float & _length) :
+	box(const float & _length):
 		length(_length)
 	{
-		verticesXY.emplace_back(-length / 2, -length / 2, length / 2);
+		verticesXY.emplace_back(-length / 2,-length / 2,length / 2);
 		verticesXY.emplace_back(length / 2, -length / 2, length / 2);
 		verticesXY.emplace_back(length / 2, -length / 2, -length / 2);
 		verticesXY.emplace_back(-length / 2, -length / 2, -length / 2);
@@ -20,75 +20,83 @@ public:
 		verticesXY.emplace_back(length / 2, length / 2, -length / 2);
 		verticesXY.emplace_back(-length / 2, length / 2, -length / 2);
 		verticesXY.emplace_back(-length / 2, length / 2, length / 2);
-
+		
 		//0,1,4,0,4,7
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(20, 499, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(250, 269, 0);
-		//1, 2, 4, 2, 5, 4
-		verticesUV.emplace_back(20, 499, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(20, 499, 0);
-		verticesUV.emplace_back(250, 269, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		//7,4,6,4,5,6
-		verticesUV.emplace_back(20, 499, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(20, 499, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(250, 269, 0);
-		//0,7,6,0,6,3
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(250, 269, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(20, 499, 0);
-		//3,6,5,3,5,2
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(250, 269, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(20, 499, 0);
-		//0,3,2,0,2,1
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(20, 499, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(20, 269, 0);
-		verticesUV.emplace_back(250, 499, 0);
-		verticesUV.emplace_back(250, 269, 0);
+		verticesUV.emplace_back(20, 500, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(250, 270, 0);
+		verticesUV.emplace_back(20, 500, 0);
+		verticesUV.emplace_back(250, 270, 0);
+		verticesUV.emplace_back(20, 270, 0);
 
+		//1, 2, 4, 2, 5, 4
+		verticesUV.emplace_back(20, 500, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(250, 270, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		//7,4,6,4,5,6
+		verticesUV.emplace_back(20, 500, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(250, 270, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		//0,7,6,0,6,3
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(250, 270, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(20, 500, 0);
+		//3,6,5,3,5,2
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(250, 270, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(20, 500, 0);
+		//0,3,2,0,2,1
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(20, 500, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(20, 270, 0);
+		verticesUV.emplace_back(250, 500, 0);
+		verticesUV.emplace_back(250, 270, 0);
 	}
 
-
-	IndexedTriangleList<Vertex>* getIndexedTriangleList() const
+	IndexedLineList getLineVertexIndexList() const
 	{
-		std::vector<Vertex> vertices;
-		std::vector<unsigned int> indices{ 0,1,4,0,4,7,
-			1, 2, 4, 2, 5, 4,
+		return 
+		{ verticesXY,{ 0,1,1,2,2,3,3,0,0,7,1,4,2,5,3,6,7,4,4,5,5,6,6,7 }};
+	}
+
+	IndexedTriangleList getTriangleVertexIndexList() const
+	{
+		return 
+		{ verticesXY,{ 0,1,4,0,4,7,
+			1,2,4,2,5,4,
 			7,4,6,4,5,6,
 			0,7,6,0,6,3,
 			3,6,5,3,5,2,
-			0,3,2,0,2,1
+			0,3,2,0,2,1} 
 		};
-		for (unsigned int i = 0; i < indices.size(); i++)
-		{
-			Vertex v_temp(verticesXY[indices[i]], verticesUV[i]);
-			vertices.emplace_back(v_temp);
-		}
-		std::string tex_img = "maptextured.jpg";
-		return new IndexedTriangleList<Vertex>{ vertices,indices, tex_img };
 	};
+
+	IndexedTriangleList getTexTriangleVertexIndexList() const
+	{
+		return { verticesUV,{ 0,1,4,0,4,7,
+			1,2,4,2,5,4,
+			7,4,6,4,5,6,
+			0,7,6,0,6,3,
+			3,6,5,3,5,2,
+			0,3,2,0,2,1}
+		};
+	}
 
 private:
 	float length;
 	std::vector<Vec3> verticesXY;
 	std::vector<Vec3> verticesUV;
-
 };
